62752e5eb72b09e229190a036c60928a5d116fa2
nathanminter1992 <nathanminter1992@penguin>
2020-09-20 11:48:29 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        
+
+            
+
+
+        }
+    }
+}
